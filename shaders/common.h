d940262337d83fc944a7e0ca334bc6e6de0d414b
Andrew MacIntyre <andrewmac@gmail.com>
2022-01-03 13:15:08 -0400

    More shader cleanup.

--- shaders/common.h
+++ shaders/common.h
@@ -63,4 +63,79 @@ float halton(unsigned int i, unsigned int d)
     return r;
 }
 
+// Uses the inversion method to map two uniformly random numbers to a three dimensional
+// unit hemisphere where the probability of a given sample is proportional to the cosine
+// of the angle between the sample direction and the "up" direction (0, 1, 0)
+inline float3 sampleCosineWeightedHemisphere(float2 u)
+{
+    float phi = 2.0f * M_PI_F * u.x;
+    
+    float cos_phi;
+    float sin_phi = sincos(phi, cos_phi);
+    
+    float cos_theta = sqrt(u.y);
+    float sin_theta = sqrt(1.0f - cos_theta * cos_theta);
+    
+    return float3(sin_theta * cos_phi, cos_theta, sin_theta * sin_phi);
+}
+
+// Maps two uniformly random numbers to the surface of a two-dimensional area light
+// source and returns the direction to this point, the amount of light which travels
+// between the intersection point and the sample point on the light source, as well
+// as the distance between these two points.
+inline void sampleAreaLight(constant AreaLight & light,
+                            float2 u,
+                            float3 position,
+                            thread float3 & lightDirection,
+                            thread float3 & lightColor,
+                            thread float & lightDistance)
+{
+    // Map to -1..1
+    u = u * 2.0f - 1.0f;
+    
+    // Transform into light's coordinate system
+    float3 samplePosition = light.position +
+                            light.right * u.x +
+                            light.up * u.y;
+    
+    // Compute vector from sample point on light source to intersection point
+    lightDirection = samplePosition - position;
+    
+    lightDistance = length(lightDirection);
+    
+    float inverseLightDistance = 1.0f / max(lightDistance, 1e-3f);
+    
+    // Normalize the light direction
+    lightDirection *= inverseLightDistance;
+    
+    // Start with the light's color
+    lightColor = light.color;
+    
+    // Light falls off with the inverse square of the distance to the intersection point
+    lightColor *= (inverseLightDistance * inverseLightDistance);
+    
+    // Light also falls off with the cosine of angle between the intersection point and
+    // the light source
+    lightColor *= saturate(dot(-lightDirection, light.forward));
+}
+
+// Aligns a direction on the unit hemisphere such that the hemisphere's "up" direction
+// (0, 1, 0) maps to the given surface normal direction
+inline float3 alignHemisphereWithNormal(float3 sample, float3 normal) {
+    // Set the "up" vector to the normal
+    float3 up = normal;
+    
+    // Find an arbitrary direction perpendicular to the normal. This will become the
+    // "right" vector.
+    float3 right = normalize(cross(normal, float3(0.0072f, 1.0f, 0.0034f)));
+    
+    // Find a third vector perpendicular to the previous two. This will be the
+    // "forward" vector.
+    float3 forward = cross(right, up);
+    
+    // Map the direction on the unit hemisphere to the coordinate system aligned
+    // with the normal.
+    return sample.x * right + sample.y * up + sample.z * forward;
+}
+
 #endif // SHADERS_COMMON_HEADER_GUARD
