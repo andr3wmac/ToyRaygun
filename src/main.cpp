ff6c16d3c5eb75678cfb8d98b636464ce79e88bf
Andrew MacIntyre <andrewmac@gmail.com>
2022-01-03 18:29:18 -0400

    Cleanup.

--- src/main.cpp
+++ src/main.cpp
@@ -6,88 +6,60 @@
 #include "engine/Platform.h"
 #include "engine/Renderer.h"
 #include "engine/Scene.h"
+
 using namespace toyraygun;
 
+#include <iostream>
 #include <bx/math.h>
-
-Scene* createCornellBoxScene()
-{
-    Scene* scene = new Scene();
-    
-    float* transform = new float[16];
-    
-    // Short box
-    bx::mtxSRT(transform, 0.6f, 0.6f, 0.6f, 0.0f, 0.3f, 0.0f, 0.3275f, 0.3f, 0.3725f);
-    scene->addCube(bx::Vec3(0.725f, 0.71f, 0.68f), transform);
-    
-    // Tall box
-    bx::mtxSRT(transform, 0.6f, 1.2f, 0.6f, 0.0f, -0.3f, 0.0f, -0.335f, 0.6f, -0.29f);
-    scene->addCube(bx::Vec3(0.725f, 0.71f, 0.68f), transform);
-    
-    // Ceiling
-    bx::mtxSRT(transform, 2.0f, 2.0f, 2.0f, 0.0f, 0.0f, bx::kPi, 0.0f, 1.0f, 0.0f);
-    scene->addPlane(bx::Vec3(0.725f, 0.71f, 0.68f), transform);
-    
-    // Floor
-    bx::mtxSRT(transform, 2.0f, 2.0f, 2.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
-    scene->addPlane(bx::Vec3(0.725f, 0.71f, 0.68f), transform);
-    
-    // Left Wall
-    bx::mtxSRT(transform, 2.0f, 2.0f, 2.0f, 0.0f, 0.0f, bx::kPi / 2.0f, 0.0f, 1.0f, 0.0f);
-    scene->addPlane(bx::Vec3(0.63f, 0.065f, 0.05f), transform);
-    
-    // Right Wall
-    bx::mtxSRT(transform, 2.0f, 2.0f, 2.0f, 0.0f, 0.0f, -bx::kPi / 2.0f, 0.0f, 1.0f, 0.0f);
-    scene->addPlane(bx::Vec3(0.14f, 0.491f, 0.05f), transform);
-    
-    // Back Wall
-    bx::mtxSRT(transform, 2.0f, 2.0f, 2.0f, -bx::kPi / 2.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
-    scene->addPlane(bx::Vec3(0.725f, 0.71f, 0.68f), transform);
-    
-    // Light source
-    bx::mtxSRT(transform, 0.5f, 1.98f, 0.5f, 0.0f, 0.0f, bx::kPi, 0.0f, 1.0f, 0.0f);
-    scene->addAreaLight(bx::Vec3(1.0f, 1.0f, 1.0f), transform);
-    
-    delete[] transform;
-
-    return scene;
-}
+#include "cornellBox.h"
 
 int main (int argc, char *args[])
 {
+    // Initialize platform and window.
     Platform* platform = new Platform();
     platform->init(1024, 768);
-    
-    Shader* rtShader = Platform::createShader();
-    if (rtShader->load("Raytracing"))
-    {
-        rtShader->addFunction("raygen", ShaderFunctionType::RayGen);
-        rtShader->addFunction("primaryHit", ShaderFunctionType::ClosestHit);
-        rtShader->addFunction("primaryMiss", ShaderFunctionType::Miss);
-        rtShader->addFunction("shadowHit", ShaderFunctionType::ShadowHit);
-        rtShader->addFunction("shadowMiss", ShaderFunctionType::ShadowMiss);
 
-        if (!rtShader->compile())
-        {
-            return -1;
-        }
-    }
-    else {
-        // Print error.
-    }
-
+    // Setup renderer with automatic selection based on platform.
     Renderer* renderer = Platform::createRenderer();
     if (!renderer->init(platform))
     {
+        std::cout << "Failed to initialize platform." << std::endl;
         return -1;
     }
-    renderer->setRaytracingShader(rtShader);
+    
+    // Setup camera.
     renderer->setCameraPosition(bx::Vec3(0.0f, 1.0f, 3.38f));
     renderer->setCameraLookAt(bx::Vec3(0.0f, 1.0f, -1.0f));
+    
+    // Setup raytracing shader.
+    Shader* rtShader = Platform::createShader();
+    if (rtShader->load("Raytracing"))
+    {
+        rtShader->addFunction("raygen",      ShaderFunctionType::RayGen);
+        rtShader->addFunction("primaryHit",  ShaderFunctionType::ClosestHit);
+        rtShader->addFunction("primaryMiss", ShaderFunctionType::Miss);
+        rtShader->addFunction("shadowHit",   ShaderFunctionType::ShadowHit);
+        rtShader->addFunction("shadowMiss",  ShaderFunctionType::ShadowMiss);
 
+        if (!rtShader->compile())
+        {
+            std::cout << "Failed to compile raytracing shader." << std::endl;
+            return -1;
+        }
+    }
+    else
+    {
+        std::cout << "Failed to load raytracing shader." << std::endl;
+        return -1;
+    }
+    
+    renderer->setRaytracingShader(rtShader);
+
+    // Load cornell box scene.
     Scene* scene = createCornellBoxScene();
     renderer->loadScene(scene);
     
+    // Render loop.
     while (!platform->hasQuit())
     {
         platform->pollEvents();
